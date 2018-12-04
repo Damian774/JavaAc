ee3ebd7856ac39fe9bd7e3682f3a198c716b9272
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-04 14:50:54 +0100

    passing primitive value to method

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 public class Main {
 
-    public void changeValue(A val) {
-        val.x = 10;
+    public void changeValue(int val) {
+        val = 10;
     }
 
     public static void main(String[] args) {
         Main t = new Main();
-        A a = new A();
-        a.x = 5;
-        t.changeValue(a);
-        System.out.println(a.x); // ?
+
+        int val = 5;
+        t.changeValue(val);
+        System.out.println(val); // ?
     }
 
-    static class A{
-        int x;
-    }
 }
