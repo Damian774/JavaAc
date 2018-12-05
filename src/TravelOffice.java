12550d6006e3527d43fce39cc5540ceef552c750
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-05 14:56:05 +0100

    for and foreachloop 2d array reading

--- src/TravelOffice.java
+++ src/TravelOffice.java
@@ -3,53 +3,17 @@
 public class TravelOffice {
 
     public static void main(String[] args) {
+        int tab[][] = {{1,2},{3,4},{5,6}};
 
-        TravelOffice travelOffice = new TravelOffice();
-        Address address = new Address("STREET_NAME", "00-000", "CITY_NAME");
-        Customer customer = new Customer("CUSTOMER_NAME");
-        customer.setAddress(address);
-        Trip trip = new Trip(new Date(2018, 12, 15), new Date(2019, 1, 14), "TRIP_DESTINATION");
-
-        customer.assignTrip(trip);
-
-        travelOffice.addCustomer(customer);
-
-    }
-
-    Customer[] customerTable;
-    static int customerCount;
+        for (int[] ints : tab)
+            for (int anInt : ints) System.out.println(anInt);
 
 
-    public TravelOffice() {
-        customerTable = new Customer[2];
-    }
+        for(int i=0;i<tab.length;i++)
+            for(int j=0;j<tab[i].length;j++)
+                System.out.println(tab[i][j]);
 
-    public void addCustomer(Customer customer) {
-        customerCount++;
-        if (customerCount > customerTable.length) {
-            Customer[] customerNewTable = new Customer[customerTable.length+1];
-           for(int i=0;i<customerTable.length;i++){
-               customerNewTable[i] = customerTable[i];
-           }
-            customerTable = new Customer[customerNewTable.length];
-           customerTable = customerNewTable.clone();
-        } else {
-            customerTable[customerCount - 1] = customer;
-        }
 
-    }
-
-    public int getCustomerCount() {
-        return customerCount;
-    }
-
-    public String getInfo() {
-        String info = "Total clients [" + customerCount + "]\n\tClient list: \n\n";
-        for (Customer customer : customerTable) {
-            info += "Client number [" + customerCount + "]\n\n";
-            customer.getInfo();
-        }
-        return info;
     }
 }
 
