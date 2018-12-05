892e4a70d125b9db9e0d8bbaaf19af8d8f73d837
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-05 08:21:14 +0100

    hello world in console

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import java.util.Random;
-
 public class Main {
 
-    public static void main(String[] args) {
-        Random random = new Random();
-        for(int i=0;i<100;i++) {
-            int randomNumber = random.nextInt(6);
-            System.out.println(randomNumber+1);
-        }
+    public static void main(String [] args) {
+        System.out.println(("Hello from the console"));
     }
 }
 
