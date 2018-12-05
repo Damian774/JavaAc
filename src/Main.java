7b9071957a175c48b68b8e8d1233fc6e17b23a1c
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-05 13:56:05 +0100

    Dividing by 0 / 0.0

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = -8;
+        int suma = x +3;
+
+        x = -x;
+
+        double iloraz1 = x/4;
+        double iloraz2 = x/4.0;
+//        double iloraz3 = x/0;
+        double iloraz4 = x/0.0;
+        double iloraz5 = 0/0.0;
+
+        int mod1 = x%4;
+        int mod2 = -x%4;
+        int mod3= -x%-4;
+
+    }
+
+}
+
+
+
+
+
