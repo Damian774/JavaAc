3c1a3de29029c237aff113aef2cbc5e84084df00
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-05 14:00:51 +0100

    checking postincrementation changes

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        int x = -8;
-        int suma = x +3;
+        int x = 5;
+        int y = x++;
+        System.out.println(x);
+        System.out.println(y);
 
-        x = -x;
-
-        double iloraz1 = x/4;
-        double iloraz2 = x/4.0;
-//        double iloraz3 = x/0;
-        double iloraz4 = x/0.0;
-        double iloraz5 = 0/0.0;
-
-        int mod1 = x%4;
-        int mod2 = -x%4;
-        int mod3= -x%-4;
+        int a =5;
+        a=a++;
+        System.out.println(a);
 
     }
 
