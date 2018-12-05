95dd9341321400fde88e64d4793157e023a99079
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-05 10:02:52 +0100

    added constructor with arguments

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
+
+    String a;
+    public Hello(String a){
+        this.a = a;
+    }
+
     public void sayHello(){
-        System.out.println("Hello");
+        System.out.println(a);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        Hello hello = new Hello();
+        Hello hello = new Hello("Hello constructor");
+        Hello hello1 = new Hello("Hello constructor 1");
+        Hello hello2 = new Hello("Hello constructor 2");
         hello.sayHello();
+        hello1.sayHello();
+        hello2.sayHello();
     }
 
 
