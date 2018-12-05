3557bc24ff71e7315d823fd30a5ba6687ed2de56
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-05 10:33:59 +0100

    Static block/normal block/constructor

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        Hello hello = new Hello("Hello constructor");
-        Hello hello1 = new Hello("Hello constructor 1");
-        Hello hello2 = new Hello("Hello constructor 2");
-        hello.sayHello();
-        hello1.sayHello();
-        hello2.sayHello();
+
+        new MyType();
     }
+}
+
+class MyType {
 
 
+    MyType(){
+        System.out.println("constructor");
+    }
+    {
+        System.out.println("code block");
+    }
+    static{
+        System.out.println("Static block");
+    }
 }
 
 
