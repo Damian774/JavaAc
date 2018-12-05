805948ec4ea575e8ec617733429205637c34d86d
Damian R <43865059+Damian774@users.noreply.github.com>
2018-12-05 02:32:01 +0100

    added getters and setters $

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 public class Main {
 
     public void changeValue(A val) {
-        val.x = 10;
+        val.setX(10);
     }
 
     public static void main(String[] args) {
         Main t = new Main();
         A a = new A();
-        a.x = 5;
+        a.setX(5);
         t.changeValue(a);
-        System.out.println(a.x); // ?
+        System.out.println(a.getX()); // ?
     }
 
     static class A{
         int x;
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
     }
 }
