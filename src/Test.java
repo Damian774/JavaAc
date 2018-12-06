37e81d4a2623bf90b932d780c4631f689605fe3a
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-06 15:01:26 +0100

    Added createDate function based on date text and dividers

--- src/Test.java
+++ src/Test.java
@@ -11,7 +11,9 @@ public class Test {
         customer1.setAddress(address);
         customer2.setAddress(address);
 
-        DomesticTrip domeseticTrip = new DomesticTrip(new Date(2018, 12, 15), new Date(2019, 1, 14), "DOMESTIC_TRIP_DESTINATION",1000,200);
+        Date d = Date.createDate("2016:10:30",":");
+
+        DomesticTrip domeseticTrip = new DomesticTrip(Date.createDate("2016-12-21","-"),new Date(2018, 12, 15), "DOMESTIC_TRIP_DESTINATION",1000,200);
         AbroadTrip abroadTrip = new AbroadTrip(new Date(2018, 12, 15), new Date(2019, 1, 14), "ABROAD_TRIP_DESTINATION",1000,100);
 
         customer1.assignTrip(domeseticTrip);
@@ -21,7 +23,6 @@ public class Test {
         travelOffice.addCustomer(customer2);
 
         System.out.println(travelOffice);
-
 
     }
 }
