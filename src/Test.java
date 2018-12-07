b9d841f7174df5ddea09eb09f62419dd1e910969
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-07 12:01:46 +0100

    decor change

--- src/Test.java
+++ src/Test.java
@@ -1,37 +1,34 @@
 public class Test {
 
     public static void main(String[] args){
-        Vehicle [] vehicles = {new SmallBoat(), new Train()};
-        for(Vehicle v : vehicles){
-            v.start();
-        }
+
     }
 
 }
 
-abstract class Vehicle{
-    void start(){
-        startEngine();
-        horn();
-        move();
-    }
-
-    void move(){ System.out.println("VEHICLE MOVE"); }
-    abstract void horn();
-    void startEngine(){ System.out.println("ENGINE START"); }
-}
-
-abstract class Ship extends Vehicle{ }
-
-class Train extends Vehicle{
+class LaserJet extends Equipment implements Printer {
     @Override
-    void horn() {System.out.println("++++ TRAIN HORN SOUND"); }
-}
+    public void print() {
 
-class SmallBoat extends Ship{
+    }
 
     @Override
-    void horn() {
-        System.out.println("#### SMALL BOAT HORN SOUND"); }
-    }
+    public void write() {
 
+    }
+}
+
+class Equipment{}
+
+
+
+
+interface Printer extends Memory, OutputDevice {
+    void print();
+}
+ interface OutputDevice {
+    void write();
+}
+ interface Memory {
+    void write();
+}
