e41ad3e82160f2eb5d53522b4f57b2ef9f27a485
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-07 12:18:33 +0100

    decor in class Square

--- src/Test.java
+++ src/Test.java
@@ -1,34 +1,91 @@
+
 public class Test {
 
     public static void main(String[] args){
-
+        RegularShape[] shapes = new RegularShape[3];
+        shapes[0] = new Triangle(1);
+        shapes[1] = new Square(1);
+        shapes[2] = new Circle(1);
+        for (RegularShape shape: shapes) {
+            shape.printShapeInfo();
+        }
     }
 
 }
 
-class LaserJet extends Equipment implements Printer {
+ interface RegularShape {
+    // metody abstrakcyjne
+    double getArea();
+    double getCircumference();
+    // metoda z implementacja domyslna
+    default void printShapeInfo() {
+        String name = getClass().getSimpleName();
+        double circ = getCircumference();
+        double area = getArea();
+        System.out.printf("%8s [circ. = %5.2f, area = %5.2f]%n",
+                name, circ, area);
+    }
+    static void printInfo(RegularShape[] list){
+        for(RegularShape shape: list) shape.printShapeInfo();
+    }
+
+}
+
+
+class Triangle implements RegularShape{
+
+    double a;
+    public Triangle(double a) {
+        this.a = a;
+    }
+
     @Override
-    public void print() {
+    public double getArea() {
+        return (a*a)/2;
+    }
+
+    @Override
+    public double getCircumference() {
+        return 3*a;
+    }
+}
+
+class Square implements RegularShape {
+    double a;
+
+    public Square(double a) {
+        this.a =a;
 
     }
 
     @Override
-    public void write() {
+    public double getArea() {
+        return a*a;
+    }
 
+    @Override
+    public double getCircumference() {
+        return 4*a;
     }
 }
 
-class Equipment{}
+class Circle implements RegularShape{
+    double r;
 
+    public Circle(double r) {
+        this.r = r;
+    }
 
+    @Override
+    public double getArea() {
+        return Math.PI*r*r;
+    }
 
-
-interface Printer extends Memory, OutputDevice {
-    void print();
+    @Override
+    public double getCircumference() {
+        return 2*Math.PI*r;
+    }
 }
- interface OutputDevice {
-    void write();
-}
- interface Memory {
-    void write();
-}
+
+
+
