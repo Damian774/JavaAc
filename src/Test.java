a611d3c0c32a48b5b537aba9eb9d15d64dcb44c3
Damian <damian.m.krzyzaniak@gmail.com>
2018-12-10 09:53:27 +0100

    Anonymous class creation

--- src/Test.java
+++ src/Test.java
@@ -1,30 +1,44 @@
 public class Test {
 
     public static void main(String[] args){
-        Vehicle v1 = new Vehicle() {
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car("car1");
+        vehicles[1] = new Car("car2");
+        vehicles[2] = new Vehicle() {
             @Override
             public void start() {
-                System.out.println("hello from car");
+                System.out.println("Starting anonymous vehicle");
             }
-
         };
-        useVehicle(v1);
-        useVehicle(new Vehicle() {
-            @Override
-            public void start() {
-                System.out.println("hello from car");
-            }
-        });
+
+        Vehicle.startVehicles(vehicles);
+        
 
     }
-    static void useVehicle(Vehicle v){
-        v.start();
-    }
-
 
 }
+
+
+
 
 interface Vehicle{
     void start();
+
+    static void startVehicles(Vehicle[] vehicles){
+        for(Vehicle v : vehicles){
+            v.start();
+        }}
 }
 
+class Car implements Vehicle{
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Starting car");
+    }
+}
